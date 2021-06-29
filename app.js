6382a2d114156fb1ba020166a85c1aabadc0eedb
DESKTOP-A4UPJPS\tranm <tranminhthien1199@gmail.com>
2021-06-29 17:50:41 +0700

    to chuc lai thu muc views

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 
 
-
 const app = express();
-
 
 app.engine('hbs', exphbs({
     defaultLayout: 'main.hbs',
@@ -13,6 +11,9 @@ app.engine('hbs', exphbs({
     partialsDir: 'views/_partials'
 }));
 app.set('view engine', 'hbs');
+
+
+
 
 
 app.get('/', function (req, res) {
